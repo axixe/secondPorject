54fe1857f914ac2c04c21908306227649c83d39c
Alexander <s1ngleez@yandex.ru>
2024-07-26 22:36:18 +0300

    update the site

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -20,7 +6,8 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        'Алфавит'
     ]
 };
 
@@ -28,6 +15,19 @@ const adWrapper = document.querySelector('.promo__adv'),
       ads = adWrapper.querySelectorAll('img'),
       bg = document.querySelector('.promo__bg'),
       movieList = document.querySelector('.promo__interactive-list');
+
+document.querySelector('button').addEventListener('click', (e) => {
+    let inputValue = document.querySelector('.adding__input').value;
+    
+    e.preventDefault();
+
+    if (inputValue.length > 20) {
+        inputValue = `${inputValue.substr(0, 20)}...`;
+    }
+
+    movieDB.movies.push(inputValue);
+    reloadMovieList();
+});
 
 ads.forEach(item => {
     item.remove();
@@ -37,14 +37,33 @@ document.querySelector('.promo__genre').textContent = 'ДРАМА';
 
 bg.style.background = 'url("../img/bg.jpg")';
 
-movieList.innerHTML = '';
+reloadMovieList();
 
-movieDB.movies.sort();
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i + 1}) ${film}
-        <div class="delete"></div>
-    </li>
-    `;
+
+function reloadMovieList() {
+    movieList.innerHTML = '';
+
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}) ${film}
+            <div class="delete"></div>
+        </li>
+        `;
+    })
+
+    if (document.querySelector('input[type="checkbox"]').checked) {
+        console.log('Добавляем любимый фильм!');
+    }
+
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        movieDB.movies.splice(i,1);
+        reloadMovieList();
+    });
 })
+}
